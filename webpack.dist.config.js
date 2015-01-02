8473d9be9fecc9ce721de49f1af33dc54d5a93d4
Siddhartha Gudipati <siddu.siddhartha@gmail.com>
2015-01-01 21:57:09 -0800

    Fixing the dist config

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -47,9 +47,18 @@ module.exports = {
       test: /\.jsx$/,
       loader: 'jsx-loader?harmony'
     }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
+      test: /\.less$/,
+      loader: "style-loader!css-loader!less-loader?strictMath&cleancss"
+    },
+    {
+      test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "url-loader?limit=10000&minetype=application/font-woff"
+    },
+    {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "url-loader?limit=81921"
+    },
+    {
       test: /\.(png|jpg)$/,
       loader: 'url-loader?limit=8192'
     }]
