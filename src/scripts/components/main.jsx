bd8261b878484cf9a1fd45e7f28cf8036ab4f179
Siddhartha Gudipati <siddu.siddhartha@gmail.com>
2015-01-01 23:11:49 -0800

    Adding the route

--- src/scripts/components/main.jsx
+++ src/scripts/components/main.jsx
@@ -6,6 +6,8 @@ var {DefaultRoute, Route, Routes} = require('react-router');
 
 React.renderComponent((
   <Routes location="history">
+    <Route path="/cc/" handler={CSApp}>
+    </Route>
     <Route path="/" handler={CSApp}>
     </Route>
   </Routes>
